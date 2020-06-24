d8104016a420c31543a635871a6a79cca68d4820
Aykut Gedik <aykut@crunchbase.com>
2020-06-23 22:00:54 -0700

    c

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/admin', function (req, res, next) {
-    re.json({app: "healthy"})
+router.get('/', function (req, res, next) {
+    res.json({
+        service: process.env.SERVICE_NAME,
+        environment: process.env.ENVIRONMENT_NAME,
+        pod: process.env.POD_NAME,
+        image: process.env.IMAGE_TAG,
+    })
 });
 
 module.exports = router;
